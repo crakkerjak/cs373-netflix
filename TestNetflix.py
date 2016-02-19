3db1ccdb353f6e86e6ac6f77c49e25d28b73308c
crakkerjak <chris.timaeus@gmail.com>
2016-02-19 15:18:14 -0600

    Added rmse tests.

--- TestNetflix.py
+++ TestNetflix.py
@@ -13,35 +13,71 @@ from io import StringIO
 from unittest import main, \
                      TestCase
 
-from Netflix import \
-  netflix_solve
+from Netflix import rmse, \
+                    print_rmse, \
+                    netflix_predict, \
+                    netflix_print, \
+                    load_data, \
+                    netflix_solve
 
 
 # -----------
 # TestNetflix
 # -----------
 class TestNetflix(TestCase):
-  # -------------
-  # netflix_solve
-  # -------------
 
-  # test desc
-  def test_netflix_solve_1(self):
-    pass
-    # s    = "1 10\n"
-    # i, j = netflix_read(s)
-    # self.assertEqual(i,  1)
-    # self.assertEqual(j, 10)
 
-  # empty input. should skip and keep going.
-  def test_netflix_solve_2(self):
-    pass
-    # s    = ""
-    # self.assertRaises(ValueError, collatz_read, s)
+    # ----
+    # rmse
+    # ----
+
+    # 0-error
+    def test_rmse_0(self):
+        n1, n2 = [1, 2, 3], [1, 2, 3]
+        r = rmse(n1, n2)
+        self.assertEqual(r, 0)
+
+    # off by 1
+    def test_rmse_1(self):
+        n1, n2 = [1, 2, 3], [2, 3, 4]
+        r = rmse(n1, n2)
+        self.assertEqual(r, 1)
+
+    # lists reversed
+    def test_rmse_2(self):
+        n1, n2 = [2, 3, 4], [1, 2, 3]
+        r = rmse(n1, n2)
+        self.assertEqual(r, 1)
+
+    # lists of length 1
+    def test_rmse_3(self):
+        n1, n2 = [1], [1]
+        r = rmse(n1, n2)
+        self.assertEqual(r, 0)
+
+    # negative numbers
+    def test_rmse_4(self):
+        n1, n2 = [1, 1, 1], [-1, -1, -1]
+        r = rmse(n1, n2)
+        self.assertEqual(r, 2)
+
+    #floats
+    def test_rmse_5(self):
+        n1, n2 = [-1.5], [1.5]
+        r = rmse(n1, n2)
+        self.assertEqual(r, 3)
+
+
+
+    # empty input. should skip and keep going.
+    def test_netflix_solve_2(self):
+        pass
+        # s        = ""
+        # self.assertRaises(ValueError, collatz_read, s)
 
 
 # ----
 # main
 # ----
 if __name__ == "__main__": # pragma: no cover
-  main()
+    main()
